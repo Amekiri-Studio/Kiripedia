acf1360c0dbbb690c8fad6ef08047189513f8635
amekiri13 <admin@amekiri.com>
2023-07-02 16:28:10 +0800

    init project

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const website_name = "Kiripedia";
+// Please set your logo location to path "./public/images", you can also use a web url, such like:
+// https://static.amekiri.com/images/logo.svg
+const website_logo = "";
+const mysql = require('mysql');
